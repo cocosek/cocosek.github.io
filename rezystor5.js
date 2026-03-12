dba2fcd806e3b7d18c72bfc45b97086e25619c73
cocosek <122176567+cocosek@users.noreply.github.com>
2026-03-12 14:42:49 +0100

    Add functions to read resistor values and calculate resistance

--- rezystor5.js
+++ rezystor5.js
@@ -1,1 +1,40 @@
+let tab=[0,0,0,1,0];
 
+function readValue1(){
+let a=document.getElementById("value1").value;
+tab[0]=parseInt(a);
+calculate();
+}
+
+function readValue2(){
+let a=document.getElementById("value2").value;
+tab[1]=parseInt(a);
+calculate();
+}
+
+function readValue3(){
+let a=document.getElementById("value3").value;
+tab[2]=parseInt(a);
+calculate();
+}
+
+function readMultiplier(){
+let a=document.getElementById("multiplier").value;
+tab[3]=parseInt(a);
+calculate();
+}
+
+function readTolerance(){
+let a=document.getElementById("tolerance").value;
+tab[4]=parseInt(a);
+calculate();
+}
+
+function calculate(){
+
+let value=(tab[0]*100 + tab[1]*10 + tab[2]) * tab[3];
+
+document.getElementById("result").innerHTML =
+"Wartość rezystancji: "+value+" Ω ±"+tab[4]+"%";
+
+}
